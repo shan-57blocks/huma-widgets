bbbee60a394b6043fc62ab386a557aa18d91bdc9
shan <shan@57blocks.com>
2023-02-10 17:09:10 +0800

    add ethereum for testing purpose

--- src/components/HumaLoading.tsx
+++ src/components/HumaLoading.tsx
@@ -2,7 +2,7 @@ import { css } from '@emotion/react'
 import { useTheme } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 
-import { HumaIcon, LoaderIcon } from './icons'
+import { LoaderIcon } from './icons'
 
 type Props = {
   fullScreen?: boolean
@@ -18,33 +18,33 @@ export function HumaLoading({
   const [count, setCount] = useState(1)
   const intervalTime = 30
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     if (count <= pathLength) {
-  //       const element = document.querySelector(
-  //         `#huma-loader-wrapper > svg > path:nth-of-type(${count})`,
-  //       )
-  //       if (element) {
-  //         // @ts-ignore
-  //         element.style.visibility = 'visible'
-  //         setCount((pre) => pre + 1)
-  //       }
-  //     } else {
-  //       const elements = document.querySelectorAll(
-  //         `#huma-loader-wrapper > svg > path`,
-  //       )
-  //       if (elements) {
-  //         // @ts-ignore
-  //         elements.forEach((element) => {
-  //           // @ts-ignore
-  //           element.style.visibility = 'hidden'
-  //         })
-  //         setCount(1)
-  //       }
-  //     }
-  //   }, intervalTime)
-  //   return () => clearInterval(interval)
-  // }, [count])
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (count <= pathLength) {
+        const element = document.querySelector(
+          `#huma-loader-wrapper > svg > path:nth-of-type(${count})`,
+        )
+        if (element) {
+          // @ts-ignore
+          element.style.visibility = 'visible'
+          setCount((pre) => pre + 1)
+        }
+      } else {
+        const elements = document.querySelectorAll(
+          `#huma-loader-wrapper > svg > path`,
+        )
+        if (elements) {
+          // @ts-ignore
+          elements.forEach((element) => {
+            // @ts-ignore
+            element.style.visibility = 'hidden'
+          })
+          setCount(1)
+        }
+      }
+    }, intervalTime)
+    return () => clearInterval(interval)
+  }, [count])
 
   const styles = {
     wrapper: css`
@@ -54,17 +54,15 @@ export function HumaLoading({
       height: 50px;
     `,
     icon: css`
-      width: 50px;
-      height: 50px;
-      /* & > path {
+      & > path {
         visibility: hidden;
-      } */
+      }
     `,
   }
 
   return (
     <div css={styles.wrapper} id='huma-loader-wrapper'>
-      <HumaIcon css={styles.icon} />
+      <LoaderIcon css={styles.icon} />
     </div>
   )
 }
